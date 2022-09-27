937b19cf2f2eaac3fcf85c122e3f405010525c7d
Tatiana Gandra <tatianagandra78@gmail.com>
2022-09-27 16:50:16 -0300

    styled home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,38 @@
+import { ReactComponent as Search } from "assets/icons/search.svg";
+import * as S from "./style";
+
 const Home = () => {
-  return(<div>Componente Home</div>);
-}
+	return (
+		<S.Home>
+			<S.HomeContent>
+				<header>
+					<S.HomeHeaderDetails>
+						<div>
+							<S.HomeHeaderDetailsLogo>Perfumaria Padron</S.HomeHeaderDetailsLogo>
+							<S.HomeHeaderDetailsDate>
+								Aqui ficará a data
+							</S.HomeHeaderDetailsDate>
+						</div>
+						<S.HomeHeaderDetailsSearch>
+							<Search />
+							<input type="text" placeholder="Procurar perfume" />
+						</S.HomeHeaderDetailsSearch>
+					</S.HomeHeaderDetails>
+				</header>
+				<div>
+					<S.HomeProductTitle>
+						<b>Perfume</b>
+					</S.HomeProductTitle>
+					<S.HomeProductList>
+						<p>Lista de produtos aqui</p>
+					</S.HomeProductList>
+				</div>
+			</S.HomeContent>
+			<aside>
+				<p>Detalhes dos pedidos aqui</p>
+			</aside>
+		</S.Home>
+	);
+};
 
 export default Home;
