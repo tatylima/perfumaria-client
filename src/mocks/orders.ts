9188ffed2de3b4fc4944b491ece9925c37b6d7ab
Tatiana Gandra <tatianagandra78@gmail.com>
2022-09-27 16:21:13 -0300

    create order e product

---
+++ src/mocks/orders.ts
@@ -0,0 +1,35 @@
+export const orders = [
+	{
+		product: {
+			id:          '354da843-18cb-4467-8c8f-45e0f594befd',
+			name:        'Paris',
+			description: 'Perfume Feminino 30ml',
+			image:       'assets/images/Perfumeparis.png',
+			price:       299.99,
+		},
+		quantity: 1,
+		observation: '',
+	},
+	{
+		product: {
+			id:          '69b772ce-815f-4d59-a7e8-fc3ae949338e',
+			name:        'Ferragamo',
+			description: 'Perfume masculino',
+			image:       'assets/images/Salvatoreferragamo.png',
+			price:       537.00,
+		},
+		quantity: 3,
+		observation: '',
+	},
+	{
+		product: {
+			id:          '9706e438-56ee-4ad5-b140-48fc2abcad81',
+			name:        'Narciso Rodrigues',
+			description: 'Perfume Masculino',
+			image:       'assets/images/narcisorodrigues.jpg',
+			price:       478.00,
+		},
+		quantity: 2,
+		observation: '',
+	},
+];
---
+++ src/mocks/products.ts
@@ -0,0 +1,44 @@
+export const products = [
+	{
+		id:          '354da843-18cb-4467-8c8f-45e0f594befd',
+			name:        'Paris',
+			description: 'Perfume Feminino 30ml',
+			image:       'assets/images/Perfumeparis.png',
+			price:       299.99,
+	},
+	{
+		id:          '69b772ce-815f-4d59-a7e8-fc3ae949338e',
+		name:        'Ferragamo',
+		description: 'Perfume masculino',
+		image:       'assets/images/Salvatoreferragamo.png',
+		price:       537.00,
+	},
+	{
+		id:         '9706e438-56ee-4ad5-b140-48fc2abcad81',
+		name:        'Narciso Rodrigues',
+		description: 'Perfume Masculino',
+		image:       'assets/images/narcisorodrigues.jpg',
+		price:       478.00,
+	},
+	{
+		id:          'a9f96dd4-c812-4683-afac-8b3b570dea34',
+		name:        'Kenzo',
+		description: 'Perfume Masculino',
+		image:       '/assets/images/Kenzo.png',
+		price:       399.99,
+	},
+	{
+		id:          'c388fdb9-f287-430a-be1d-c2c24e99977c',
+		name:        'Eau de pamplemousse rose',
+		description: 'Perfume Feminino',
+		image:       '/assets/images/pamplemousserose.png',
+		price:       432.00,
+	},
+	{
+		id:          'd294e03e-2992-40a1-a02c-ba41feff59dc',
+		name:        'Channel',
+		description: 'Perfume Feminino',
+		image:       '/assets/images/chanel.png',
+		price:       379.00,
+	}
+]
