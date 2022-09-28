f1c12f2c8ac3ba74ac5f01c4ef477e578afb9a48
Tatiana Gandra <tatianagandra78@gmail.com>
2022-09-27 21:33:34 -0300

    add luxon

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,41 +1,51 @@
-import Menu from "components/Menu";
-import { ReactComponent as Search } from "assets/icons/search.svg";
-import * as S from "./style";
-import { RoutePath } from "types/routes";
-import { navigationItems } from "data/navigation";
+import Menu from 'components/Menu';
+import { ReactComponent as Search } from 'assets/icons/search.svg';
+import * as S from './style';
+import { RoutePath } from 'types/routes';
+import { navigationItems } from 'data/navigation';
+import { DateTime } from "luxon";
+
 const Home = () => {
-	return (
-		<S.Home>
-			<Menu active={RoutePath.HOME} navItems={navigationItems} />
-			<S.HomeContent>
-app/src/navigation.tsx				<header>
-					<S.HomeHeaderDetails>
-						<div>
-							<S.HomeHeaderDetailsLogo>Perfumaria Padron</S.HomeHeaderDetailsLogo>
-							<S.HomeHeaderDetailsDate>
-								Aqui ficará a data
-							</S.HomeHeaderDetailsDate>
-						</div>
-						<S.HomeHeaderDetailsSearch>
-							<Search />
-							<input type="text" placeholder="Procurar perfume" />
-						</S.HomeHeaderDetailsSearch>
-					</S.HomeHeaderDetails>
-				</header>
-				<div>
-					<S.HomeProductTitle>
-						<b>Perfumes</b>
-					</S.HomeProductTitle>
-					<S.HomeProductList>
-						<p>Lista de produtos aqui</p>
-					</S.HomeProductList>
-				</div>
-			</S.HomeContent>
-			<aside>
-				<p>Detalhes dos pedidos aqui</p>
-			</aside>
-		</S.Home>
-	);
+  const dateDescription = DateTime.now().toLocaleString({
+    ...DateTime.DATE_SHORT,
+    weekday: "long",
+  });
+
+  return (
+    <S.Home>
+      <Menu active={RoutePath.HOME} navItems={navigationItems} />
+      <S.HomeContent>
+        app/src/navigation.tsx{' '}
+        <header>
+          <S.HomeHeaderDetails>
+            <div>
+              <S.HomeHeaderDetailsLogo>
+                Perfumaria Padron
+              </S.HomeHeaderDetailsLogo>
+              <S.HomeHeaderDetailsDate>
+                {dateDescription}
+              </S.HomeHeaderDetailsDate>
+            </div>
+            <S.HomeHeaderDetailsSearch>
+              <Search />
+              <input type="text" placeholder="Procurar perfume" />
+            </S.HomeHeaderDetailsSearch>
+          </S.HomeHeaderDetails>
+        </header>
+        <div>
+          <S.HomeProductTitle>
+            <b>Perfumes</b>
+          </S.HomeProductTitle>
+          <S.HomeProductList>
+            <p>Lista de produtos aqui</p>
+          </S.HomeProductList>
+        </div>
+      </S.HomeContent>
+      <aside>
+        <p>Detalhes dos pedidos aqui</p>
+      </aside>
+    </S.Home>
+  );
 };
 
 export default Home;
