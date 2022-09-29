99dae15c4a4c8bd7ed24158a88e40eba44239d71
Tatiana Gandra <tatianagandra78@gmail.com>
2022-09-29 16:25:04 -0300

    modification router

--- src/router.tsx
+++ src/router.tsx
@@ -2,20 +2,24 @@ import { Routes, Route } from "react-router-dom";
 import { RoutePath } from "types/routes";
 import Home from "pages/Home/index";
 import Login from "pages/Login";
-import Settings from "pages/Settings";
 import ManageProducts from "components/ManageProducts";
 import ManageUsers from "components/ManageUsers";
+import Settings from "pages/Settings";
 
 const Router = () => {
-	return (
-		<Routes>
-			<Route path={RoutePath.LOGIN} element={<Login />} />
-			<Route path={RoutePath.HOME} element={<Home />} />
-			<Route path={RoutePath.SETTINGS} element={<Settings />} />
-			<Route path={RoutePath.SETTINGS_PRODUCTS} element={<ManageProducts />} />
-			<Route path={RoutePath.SETTINGS_USERS} element={<ManageUsers />} />
-		</Routes>
-	);
+  return (
+    <Routes>
+      <Route path={RoutePath.LOGIN} element={<Login />} />
+      <Route path={RoutePath.HOME} element={<Home />} />
+      <Route path={RoutePath.SETTINGS} element={<Settings />}>
+        <Route
+          path={RoutePath.SETTINGS_PRODUCTS}
+          element={<ManageProducts />}
+        />
+        <Route path={RoutePath.SETTINGS_USERS} element={<ManageUsers />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default Router;
