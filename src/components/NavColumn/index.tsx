e6936e583b424cf2e5f26a333d60882942f3a494
Tatiana Gandra <tatianagandra78@gmail.com>
2022-09-29 15:40:59 -0300

    add navColum

---
+++ src/components/NavColumn/index.tsx
@@ -0,0 +1,45 @@
+import { ReactComponent as Market } from "assets/icons/market.svg";
+import { ReactComponent as Info } from "assets/icons/info.svg";
+import { ReactComponent as Users } from "assets/icons/users.svg";
+import * as S from "./style";
+import { RoutePath } from "types/routes";
+import NavColumnItem from "components/NavColumnItem";
+import { HTMLAttributes } from "react";
+
+type NavColumnType = HTMLAttributes<HTMLDivElement>;
+
+type NavColumnProps = {
+  activeRoute: RoutePath;
+} & NavColumnType;
+
+const NavColumn = ({ activeRoute }: NavColumnProps) => {
+  const items = [
+    {
+      icon: <Info />,
+      title: "Gerenciar Produtos",
+      subtitle: "Edite os perfumes, preços e etc.",
+      navigation: RoutePath.SETTINGS_PRODUCTS,
+    },
+    {
+      icon: <Users />,
+      title: "Gerenciar Usuários",
+      subtitle: "Gerencie o acesso ao sistema",
+      navigation: RoutePath.SETTINGS_USERS,
+    },
+  ];
+  return (
+    <S.NavColumn>
+      {items.map((item, key) => (
+        <NavColumnItem
+          active={item.navigation === activeRoute}
+          icon={item.icon}
+          title={item.title}
+          key={key}
+          subtitle={item.subtitle}
+        />
+      ))}
+    </S.NavColumn>
+  );
+};
+
+export default NavColumn;
