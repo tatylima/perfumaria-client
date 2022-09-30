d0639169edd20423c6b1d9b6487761b35635a567
Tatiana Gandra <tatianagandra78@gmail.com>
2022-09-30 14:20:47 -0300

    modification BoxLogin

--- src/components/BoxLogin/index.tsx
+++ src/components/BoxLogin/index.tsx
@@ -1,20 +1,50 @@
-import logo from "assets/images/logoperfumaria.png";
-import * as S from "./style";
-import ButtonLarge from "components/ButtonLarge";
+import logo from 'assets/images/logoperfumaria.png';
+import ButtonLarge from 'components/ButtonLarge';
+import {HTMLAttributes,useState} from 'react';
+import * as S from './style';
 
-const BoxLogin = () => {
-	return (
-		<S.BoxLogin>
-			<S.BoxLoginLogo>
-				<S.BoxLoginLogoImage src={logo} alt="Logo" />
-			</S.BoxLoginLogo>
-			<S.BoxLoginForm>
-				<input type="text" placeholder="E-mail" />
-				<input type="password" placeholder="Senha" />
-				<ButtonLarge value="Entrar" type="button" />
-			</S.BoxLoginForm>
-		</S.BoxLogin>
-	);
+type BoxLoginType = HTMLAttributes<HTMLDivElement>;
+
+export type BoxLoginProps = {
+  onSubmitData: (data: { nickname: string; password: string }) => void;
+  errorMessage: string;
+} & BoxLoginType;
+
+const BoxLogin = ({ onSubmitData, errorMessage }: BoxLoginProps) => {
+  const [nickname, setNickname] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (): void => {
+    const data = { nickname, password };
+    onSubmitData(data);
+  };
+
+  return (
+    <S.BoxLogin>
+      <S.BoxLoginLogo>
+        <S.BoxLoginLogoImage src={logo} alt="Logo" />
+      </S.BoxLoginLogo>
+      <S.BoxLoginForm>
+        <input
+          type="text"
+          placeholder="Nickname"
+          value={nickname}
+          onChange={({ target }) => setNickname(target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Senha"
+          value={password}
+          onChange={({ target }) => setPassword(target.value)}
+        />
+        <ButtonLarge value="Entrar" type="button" onClick={handleSubmit} />
+      </S.BoxLoginForm>
+      {Boolean(errorMessage.length) && (
+        <S.BoxLoginError>{errorMessage}</S.BoxLoginError>
+      )}
+    </S.BoxLogin>
+  );
 };
 
 export default BoxLogin;
