1e827953785968b35dc4c89190e704a0a36856bf
Tatiana Gandra <tatianagandra78@gmail.com>
2022-10-02 17:30:26 -0300

    modification home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -11,8 +11,9 @@ import Overlay from "components/Overlay";
 import CheckoutSection from "components/CheckoutSection";
 import { useNavigate } from "react-router-dom";
 import { products } from "mocks/products";
-import { orders } from "mocks/orders";
 import { ProductResponse } from "types/Product";
+import { useState } from "react";
+import { OrderItemType } from "types/OrderItemType";
 
 const Home = () => {
   const dateDescription = DateTime.now().toLocaleString({
@@ -20,9 +21,26 @@ const Home = () => {
     weekday: "long",
   });
   const navigate = useNavigate();
+
+  const [orders, setOrders] = useState<OrderItemType[]>([]);
+
   const handleNavigation = (path: RoutePath) => navigate(path);
-  const handleSelection = (product: ProductResponse) => {};
   
+  const handleSelection = (product: ProductResponse) => {
+    const existing = orders.find((i) => i.product.id === product.id);
+    const quantity = existing ? existing.quantity + 1 : 1;
+    const item: OrderItemType = { product, quantity };
+  
+  
+  const list = existing? orders.map((i) => (i.product.id === existing.product.id ? item : i)): [...orders, item];
+    setOrders(list);
+  };
+
+  const handleRemoveOrderItem = (id: string) => {
+    const filtered = orders.filter((i) => i.product.id != id);
+    setOrders(filtered);
+  };
+
   return (
     <S.Home>
        <Menu 
@@ -67,7 +85,10 @@ const Home = () => {
         </div>
       </S.HomeContent>
       <aside>
-      <OrderDetails orders={orders} />
+        <OrderDetails
+          orders={orders}
+          onRemoveItem={handleRemoveOrderItem}
+        />
       </aside>
       {/* <Overlay>
                 <CheckoutSection />
