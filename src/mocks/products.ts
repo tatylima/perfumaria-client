33d055f4568a2beff519962dcf230b3c876cab04
Tatiana Gandra <tatianagandra78@gmail.com>
2022-10-06 18:58:38 -0300

    modification designer

--- src/mocks/products.ts
+++ src/mocks/products.ts
@@ -2,45 +2,45 @@ import { ProductResponse } from "types/Product";
 
 export const products: ProductResponse[] = [
   {
-		id:          '354da843-18cb-4467-8c8f-45e0f594befd',
+		  id:          '354da843-18cb-4467-8c8f-45e0f594befd',
 			name:        'Paris',
 			description: 'Perfume Feminino 30ml',
-			image:       'https://drive.google.com/file/d/1Wk_5xvGOglewj0m0Cv-t8m6epwCkyn0v/view?usp=sharing',
+			image:       'https://i.imgur.com/VKbt3ib.png',
 			price:       299.99,
 	},
 	{
-		id:          '69b772ce-815f-4d59-a7e8-fc3ae949338e',
+		id:          '354da843-18cb-4467-8c8f-45e0f594befd',
 		name:        'Ferragamo',
 		description: 'Perfume masculino',
-		image:       'https://drive.google.com/file/d/1W_CRSXAQEjLMjFE7LomtcgZRjtdPadsz/view?usp=sharing',
+		image:       'https://i.imgur.com/fagW9Hh.png',
 		price:       537.00,
 	},
 	{
-		id:         '9706e438-56ee-4ad5-b140-48fc2abcad81',
+		id:         '354da843-18cb-4467-8c8f-45e0f594befd',
 		name:        'Narciso Rodrigues',
 		description: 'Perfume Masculino',
-		image:       'https://drive.google.com/file/d/1TYisiXVRj31HsJoy7jaBTt-A14tBkp1d/view?usp=sharing',
+		image:       'https://i.imgur.com/nvM1qGL.jpg',
 		price:       478.00,
 	},
 	{
-		id:          'a9f96dd4-c812-4683-afac-8b3b570dea34',
+		id:          '354da843-18cb-4467-8c8f-45e0f594befd',
 		name:        'Kenzo',
 		description: 'Perfume Masculino',
-		image:       'https://drive.google.com/file/d/1bXjcZTw15cHPOhCuoUoImX4BkR_MLFKh/view?usp=sharing',
+		image:       'https://i.imgur.com/yquZrIe.png',
 		price:       399.99,
 	},
 	{
-		id:          'c388fdb9-f287-430a-be1d-c2c24e99977c',
+		id:          '354da843-18cb-4467-8c8f-45e0f594befd',
 		name:        'Eau de pamplemousse rose',
 		description: 'Perfume Feminino',
-		image:       'https://drive.google.com/file/d/1Xx0nq0MtXlBdkymwHWqFOq3HM1SaK3OH/view?usp=sharing',
+		image:       'https://i.imgur.com/JALi9qY.png',
 		price:       432.00,
 	},
 	{
-		id:          'd294e03e-2992-40a1-a02c-ba41feff59dc',
+		id:          '354da843-18cb-4467-8c8f-45e0f594befd',
 		name:        'Channel',
 		description: 'Perfume Feminino',
-		image:       'https://drive.google.com/file/d/1Ct9JuJee0KK6PTr9C4sf7mJheUALI12N/view?usp=sharing',
+		image:       'https://i.imgur.com/Yoj76Kw.png',
 		price:       379.00,
 	}
 ]
