858542268578d094bfbe3550bd52d4a247059bf2
Tatiana Gandra <tatianagandra78@gmail.com>
2022-11-07 19:18:21 -0300

    modificação url

--- src/helpers/endpoints/index.tsx
+++ src/helpers/endpoints/index.tsx
@@ -4,7 +4,7 @@ import { product } from './product';
 import { user } from './user';
 
 export const endpoint = {
-    baseUrl: "https://perfumaria-server-production-fbb6.up.railway.app/",
+    baseUrl: "https://perfumaria-server-production-fbb6.up.railway.app/api",
     ...auth,
     ...user,
     ...order,
