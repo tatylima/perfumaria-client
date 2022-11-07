6ab68e606ffdffd6e89026c1096cb78208689494
Tatiana Gandra <tatianagandra78@gmail.com>
2022-11-07 14:22:59 -0300

    adicionando  interfaces referentes as entidades da API

--- src/components/CheckoutSection/index.tsx
+++ src/components/CheckoutSection/index.tsx
@@ -1,11 +1,18 @@
 import { HTMLAttributes, useState } from "react";
 import CheckboxIcon from "components/CheckboxIcon";
-import OrderConfirmation from "components/OrderConfirmation";
-import * as S from './style';
+import OrderConfirmation from "components/OrderConfirmation"
 import { ReactComponent as Card } from "assets/icons/credit-card.svg";
 import { ReactComponent as Cash } from "assets/icons/wallet.svg";
 import { OrderItemType } from "types/OrderItemType";
 import { PaymentMethod } from "types/PaymentMethod";
+import { useMutation } from "@tanstack/react-query";
+import { OrderService } from "services/OrderService";
+import { ErrorResponse } from "types/api/error";
+import { LocalStorageHelper } from "helpers/LocalStorageHelper";
+import { LocalStorageKeys } from "types/LocalStorageKeys";
+import { UserResponse } from "types/api/user";
+import { Order } from "types/api/order";
+import * as S from './style';
 
 type CheckoutSectionType = HTMLAttributes<HTMLDivElement>;
 
@@ -15,18 +22,37 @@ type CheckoutSectionProps = {
   onCloseSection: () => void;
 } & CheckoutSectionType;
 
-const CheckoutSection = ({
-  orders,
-  onOrdersChange,
-  onCloseSection,
-}: CheckoutSectionProps) => {
-  const [activeMethod, setActiveMethod] = useState<PaymentMethod>();
-  const [closing, setClosing] = useState<boolean>(false);
+const CheckoutSection = ({ orders, onOrdersChange,   onCloseSection }: CheckoutSectionProps) => {
 
-  const handleCloseSection = () => {
-    setClosing(true);
-    setTimeout(onCloseSection, 800);
+  const [activeMethod, setActiveMethod] = useState<PaymentMethod>();
+  const [closing, setClosing] = useState<boolean>(false)
+
+const closeOrder = useMutation(OrderService.create, {
+  onSuccess: (data: {} & ErrorResponse) => {
+      if (data.statusCode) {
+          return;
+      }
+      onOrdersChange([]);
+  },
+  onError: () => {
+      console.error("Erro ao fechar o pedido!");
+  }
+});
+
+const handlePaymentConfirm = () => {
+  const userId = LocalStorageHelper.get<UserResponse>(LocalStorageKeys.USER)?.id || "";
+  const orderRequest: Order = {
+      userId,
+      products: orders,
   };
+  closeOrder.mutate(orderRequest);
+}
+
+const handleCloseSection = () => {
+  setClosing(true);
+  setTimeout(onCloseSection, 800);
+}
+
 
 
   return (
